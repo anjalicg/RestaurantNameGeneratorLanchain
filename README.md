97413ced302cd91eae7b582754e21bead40fc4ed
anjali <anjalicg2001@gmail.com>
2025-01-27 06:16:40 -0800

    Working example for Langchain

---
+++ README.md
@@ -0,0 +1,12 @@
+#Retaurant Name Generator
+
+## About
+This project uses the Langchain library to get 
+restaurant name suggestions based in cuisine and
+then gets suggestions for menu items.
+
+
+## Run
+Run it via streamlit
+
+`streamlit run src/main.py`
---
+++ src/main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from util import generate_restaurant_name_and_items
+from dotenv import load_dotenv
+st.title("Restaurant Name Generator")
+cuisine = st.sidebar.selectbox("Pick a cuisine", ("Indian", "Mexican", "Arabic", "French"))
+
+load_dotenv()
+
+if cuisine:
+    response = generate_restaurant_name_and_items(cuisine)
+    st.header(response["restaurant_name"].strip())
+    menu_items = response["menu_items"].strip().split(",")
+    print(f"menu_items = {menu_items}")
+    print(f"menu_items = {type(menu_items)}")
+    st.write("**Menu items**")
+    for item in menu_items:
+        print(f"DEBUG = {item}")
+        if len(item.strip()) > 0:
+            st.write("-",item)
+
+
+
